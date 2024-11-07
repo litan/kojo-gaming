e9502cd54ac413d675fc6f8ccb577e10e715bcd0
Lalit Pant <pant.lalit@gmail.com>
2024-11-07 15:38:10 +0530

    Make white the clear color during rendering.

--- core/src/main/scala/net/kogics/kojo/picgaming/PicGdxScreen.scala
+++ core/src/main/scala/net/kogics/kojo/picgaming/PicGdxScreen.scala
@@ -67,7 +67,7 @@ abstract class PicGdxScreen extends Screen with InputProcessor {
 
       update(dt)
 
-      Gdx.gl.glClearColor(0, 0, 0, 1)
+      Gdx.gl.glClearColor(1, 1, 1, 1)
       Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
       // draw the pic scene graph
